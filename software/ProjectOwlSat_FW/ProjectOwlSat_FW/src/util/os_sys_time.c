471d4adbf5973f2c6e709f9f7d23c4bd92720a6b
Penguin <ad0097@uah.edu>
2019-10-06 23:24:32 -0500

    Added software stuff

---
+++ software/ProjectOwlSat_FW/ProjectOwlSat_FW/src/util/os_sys_time.c
@@ -0,0 +1,9 @@
+/*
+ * os_sys_time.c
+ *
+ * Created: 10/6/2019 10:52:12 PM
+ *  Author: penguin
+ */ 
+
+#include "os_sys_time.h"
+
---
+++ software/ProjectOwlSat_FW/ProjectOwlSat_FW/src/util/os_sys_time.h
@@ -0,0 +1,13 @@
+/*
+ * os_sys_time.h
+ *	
+ * Created: 10/6/2019 10:51:52 PM
+ *  Author: penguin
+ * Note: OS stands for OwlSat. Not operating system.
+ */ 
+#ifndef _OS_SYS_TIME_H_
+#define _OS_SYS_TIME_H_
+
+
+
+#endif
